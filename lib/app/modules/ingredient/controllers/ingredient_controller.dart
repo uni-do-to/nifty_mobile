6ba837f6f5ae7b6924e6850efc8ed9beaeea4450
uni-do-to <107849405+uni-do-to@users.noreply.github.com>
2023-12-06 11:08:11 +0200

    design home view [meals tab] [tab view ]

---
+++ lib/app/modules/ingredient/controllers/ingredient_controller.dart
@@ -0,0 +1,23 @@
+import 'package:get/get.dart';
+
+class IngredientController extends GetxController {
+  //TODO: Implement IngredientController
+
+  final count = 0.obs;
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+
+  void increment() => count.value++;
+}
